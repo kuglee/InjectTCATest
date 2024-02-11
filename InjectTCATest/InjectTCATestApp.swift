85af5bcd447ec1712ad91064856ed7592010231c
Gábor Librecz <kuglee@gmail.com>
2024-02-11 17:21:47 +0100

    Initial Commit

---
+++ InjectTCATest/InjectTCATestApp.swift
@@ -0,0 +1,17 @@
+//
+//  InjectTCATestApp.swift
+//  InjectTCATest
+//
+//  Created by Librecz Gábor on 11/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct InjectTCATestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
